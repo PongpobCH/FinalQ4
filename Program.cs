5ba9fc8473683da8357e271e21a80864dbc49b75
pongpob <khlangs456@gmail.com>
2022-12-09 16:26:32 +0700

    finalQ4

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    
+    
+    static void Main()
+    {
+        
+        int i = 0;
+        Queue<string> queue = new Queue<string>(); 
+        string input,input2;
+        int inputint;
+        Console.WriteLine("input = ");
+        input = Console.ReadLine();
+        
+        
+        
+        
+        queue.Push(input);
+
+        
+        if(input != "done")
+
+        {
+            Console.WriteLine("Price =");
+            input2 = Console.ReadLine();
+            Main();
+            string value = queue.Pop();
+            Console.WriteLine(value + " ({0}) ",input2 );
+        }
+
+        else
+        {
+
+            
+        }
+        
+
+        
+    
+    }
+    
+       
+}
